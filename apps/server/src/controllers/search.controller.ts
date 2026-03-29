6ca4061b92918876967ed3988514859c51e38db3
jaydev0220 <contact@hsieh-dev.us.ci>
2026-03-29 18:16:01 +0800

    feat(server): add search controller

---
+++ apps/server/src/controllers/search.controller.ts
@@ -0,0 +1,15 @@
+import type { RequestHandler } from 'express';
+import * as searchService from '../services/search.service.js';
+import type { SearchParams } from '@packages/schemas';
+
+export const search: RequestHandler = async (req, res) => {
+	const params = req.query as unknown as SearchParams;
+	const userId = req.user!.sub;
+
+	const result =
+		params.type === 'users'
+			? await searchService.searchUsers(userId, params)
+			: await searchService.searchMessages(userId, params);
+
+	res.json(result);
+};
